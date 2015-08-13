ac11da60d958b6ed37e4b7c2a7871e24fa398b2e
Kosh <kosh@codeworx.com.au>
2015-08-13 13:03:12 +1000

    Updated readme and imported replacement sslscan module (and added new but broken sslyze module, fixing now)

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ homebrew-extras
 ===============
 
 Additional homebrew formula for newer or alternate modules
+
+
+| Formula | Original Source |
+| ------------- | ------------- |
+| sslscan.rb | https://github.com/sidaf/homebrew-pentest/blob/master/sslscan.rb |
+| sslyze.rb | https://github.com/sidaf/homebrew-pentest/blob/master/sslyze.rb |
--- sslscan.rb
+++ sslscan.rb
@@ -2,22 +2,18 @@ require 'formula'
 
 class Sslscan < Formula
   homepage 'https://github.com/rbsec/sslscan'
-  url 'https://github.com/rbsec/sslscan/archive/239e01f489393f3777bddcd424072957e9ba00a5.zip'
-  sha1 'ba1968a5b09e3cf45a7b160ce8052b04a2c426b7'
+  url 'https://github.com/rbsec/sslscan/archive/1.10.0-rbsec.tar.gz'
+  sha1 '746db4d2efabf487348c9ffd407ad527a5e0fe06'
+  version '1.10.0'
 
-  # Remove hardcoded CFLAGS and LDFLAGS in Makefile
+  depends_on "openssl"
+
+  # Fix compilation and statically link against OpenSSL 1.0.2 rather than 1.0.1
   patch :DATA
 
   def install
-    if build.with? 'openssl'
-      args = %W[
-        CFLAGS=#{'-I' + Formula['openssl'].opt_prefix.to_s + '/include/'}
-        LDFLAGS=#{'-L' + Formula['openssl'].opt_prefix.to_s + '/lib/'}
-      ]
-    else
-      args = %W[]
-    end
-    system "make", *args
+    ENV.deparallelize
+    system "make"
     bin.install "sslscan"
     man1.install "sslscan.1"
   end
@@ -25,23 +21,31 @@ class Sslscan < Formula
   test do
     system "#{bin}/sslscan"
   end
-
-  depends_on "openssl" => :recommended
 end
 
 __END__
 diff --git a/Makefile b/Makefile
-index 388d916..6ba9e9b 100644
+index 8a38e4c..d18743f 100644
 --- a/Makefile
 +++ b/Makefile
-@@ -6,8 +6,8 @@ endif
- SRCS = sslscan.c
- BINPATH = /usr/bin/
- MANPATH = /usr/share/man/
--CFLAGS=-I/usr/local/ssl/include/ -I/usr/local/ssl/include/openssl/
--LDFLAGS=-L/usr/local/ssl/lib/
-+#CFLAGS=-I/usr/local/ssl/include/ -I/usr/local/ssl/include/openssl/
-+#LDFLAGS=-L/usr/local/ssl/lib/
+@@ -23,8 +23,8 @@ LIBS      = -lssl -lcrypto
+ # for static linking
+ ifeq ($(STATIC_BUILD), TRUE)
+ PWD          = $(shell pwd)/openssl
+-LDFLAGS      += -L${PWD}/
+-CFLAGS       += -I${PWD}/include/ -I${PWD}/
++LDFLAGS      = -L${PWD}/
++CFLAGS       = -I${PWD}/include/ -I${PWD}/
+ LIBS         = -lssl -lcrypto -ldl
+ GIT_VERSION  := $(GIT_VERSION)-static
+ else
+@@ -51,7 +51,7 @@ uninstall:
+  rm -f $(MANPATH)man1/sslscan.1
+ 
+ openssl/Makefile:
+- [ -d openssl -a -d openssl/.git ] && true || git clone https://github.com/openssl/openssl ./openssl && cd ./openssl && git checkout OpenSSL_1_0_1-stable
++ [ -d openssl -a -d openssl/.git ] && true || git clone https://github.com/openssl/openssl ./openssl && cd ./openssl && git checkout OpenSSL_1_0_2-stable
+ 
+ openssl/libcrypto.a: openssl/Makefile
+  cd ./openssl; ./config no-shares
 
- all: $(SRCS)
-        $(CC) -Wall ${LDFLAGS} ${SRCS} ${CFLAGS} -lssl -lcrypto -o sslscan
